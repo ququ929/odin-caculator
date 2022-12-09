dcd41e22a9b6d65099e1e8f2556b713a841fe295
ququ929 <ququ929@hotmail.com>
2022-12-09 18:59:39 +0800

    Create basic arithmetic operation functions

--- caculator.js
+++ caculator.js
@@ -13,3 +13,20 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+// call +, - ,* ,/ function depend on operator
+function operate (operator, number1, number2) {
+    switch (operator) {
+        case "+":
+            return add(number1, number2);
+        
+        case "-":
+            return subtract(number1, number2);
+        
+        case "*":
+            return multiply(number1, number2);
+
+        case "/":
+            return divide(number1,number2);
+    }
+}
